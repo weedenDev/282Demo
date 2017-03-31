613108c7ca2e19bae6a7809f189549d84ac31756
codyweeden <cw_13@live.ca>
2017-03-31 16:09:46 -0400

    in class

--- js/memes.js
+++ js/memes.js
@@ -1,2 +1,11 @@
 var memes = ["barber.png", "bst.jpeg", "cage.jpg", "cookie.png", "java.jpg", "logn.png", "money.jpg", "muppet.jpg", "ryan.png", "stack.png", "source.png", "itcrowd.jpeg", "web.jpg"];
 
+function memeIt(memes){
+	var randMeme = memes[Math.floor(Math.random() * memes.length)];
+	var img = document.createElement("img");
+	var imgShortcut ="./img/";
+	img.setAttribute("class", "img-fluid");
+	img.src = imgShortcut.concat(randMeme);
+	document.getElementById("memesGoesHere").appendChild(img);
+	
+}
